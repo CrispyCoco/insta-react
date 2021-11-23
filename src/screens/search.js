c580c1bffc5d4b137193051a81ed5e253f080141
Crispy Coco <gomezrodriguezj@udesa.edu.ar>
2021-11-22 23:54:49 -0300

    .

--- src/screens/search.js
+++ src/screens/search.js
@@ -7,6 +7,7 @@ import {
   View,
   StyleSheet,
 } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome'
 import { db, auth } from "../firebase/config";
 import Post from "../components/Post";
 
@@ -20,6 +21,7 @@ class search extends Component {
     };
   }
   search() {
+    console.log(this.state.user);
     db.collection("posts")
       .where("owner", "==", this.state.user)
       .onSnapshot((docs) => {
@@ -38,8 +40,10 @@ class search extends Component {
   }
 
   render() {
+    console.log(this.state.posts);
     return (
       <View style={styles.view}>
+        <View style={styles.search}>
         <TextInput
           style={styles.input}
           onChangeText={(text) => this.setState({ user: text })}
@@ -47,9 +51,10 @@ class search extends Component {
           keyboardType="default"
           value={this.state.user}
         />
-        <TouchableOpacity onPress={() => this.search()}>
-          <Text>Search</Text>
+        <TouchableOpacity onPress={() => this.search()} style={styles.button}>
+          <Icon name="search" color="white" size={25} style={styles.icon}/>
         </TouchableOpacity>
+        </View>
 
         <View styles={styles.list}>
         <FlatList 
@@ -71,21 +76,46 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "center",
     minHeight: "100vh",
-    width:'100%'
+    width:'100%',
   },
-  list: {
-    width: "100%",
+  search:{
+    width:'90%',
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 15,
+    alignItems: "center",
+    justifyContent: 'space-around'
   },
   input:{
     width: '80%',
     border: '1px solid black',
     borderRadius: '10px',
     paddingHorizontal: 8,
-    paddingVertical: 5, 
+    paddingVertical: 8, 
     marginVertical: 4,
     backgroundColor: 'rgba(87, 84, 95, 0.445)',
     color: 'white',
     outlineStyle: 'none'
   },
+  button:{
+    width: '18%',
+    backgroundColor: 'none',
+    borderRadius: '10px',
+    outlineStyle: 'none'
+  },
+  icon:{
+    // color: 'white',
+    // width: '100%',
+    // height: '100%',
+    textAlign: 'center',
+    // paddingTop: '20%',
+  },
+  list: {
+    flex:1,
+    // backgroundColor: 'rgb(12, 11, 14)',
+    color: 'white',
+    borderRadius: 2,
+    padding: 5,
+  }
 });
 export default search;
